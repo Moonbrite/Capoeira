216a410891df4efda9ce6d99cf951e72d44c1676
Antoine <136310380+antoine-codeur@users.noreply.github.com>
2024-12-02 14:50:15 +0100

    add home to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import {ActiveDiscussionsComponent} from './active-discussions/active-discussions.component';
 import {ActiveDiscussionComponent} from './active-discussions/active-discussion/active-discussion.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  {path: 'active-discussions', component: ActiveDiscussionsComponent,
-  children: [
-    { path:'active-discussions/:id' , component: ActiveDiscussionComponent },]
-  }
+
+    {path: '', component: HomeComponent},
+    {path: 'active-discussions', component: ActiveDiscussionsComponent,
+    children: [
+        { path:'active-discussions/:id' , component: ActiveDiscussionComponent },]
+    }
 ];
