c87e17d77002f3328db8e9e38d9798ab4f50c561
Moon Brite <josemiguelvalecosta@gmail.com>
2024-12-02 14:38:08 +0100

    Ajout du composant Home et modif nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {ActiveDicussionsComponent} from './active-discussions/active-dicussions.component';
 import {ActiveDicussionComponent} from './active-discussions/active-dicussion/active-dicussion.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'active-discussions', component: ActiveDicussionsComponent,
   children: [
     { path:'active-discussions/:id' , component: ActiveDicussionComponent },]
